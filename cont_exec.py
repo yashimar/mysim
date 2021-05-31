1b7a83d7eaf03ae46345d6ee71efe75db8c92193
yashimar <ryota.yashima.s8@dc.tohoku.ac.jp>
2021-05-31 20:50:32 +0900

    連続実行用ファイル追加

---
+++ cont_exec.py
@@ -0,0 +1,14 @@
+from core_tool import *
+
+
+def Help():
+    pass
+
+
+def Run(ct, *args):
+    ct.Run('mysim.curriculum.analyze.log.curriculum2.pouring.t1.lppour_datotaltip')
+    ct.Run('mysim.curriculum.analyze.log.curriculum2.pouring.t2.lppour_datotaltip')
+    ct.Run('mysim.curriculum.analyze.log.curriculum2.pouring.t3.lppour_datotaltip')
+    ct.Run('mysim.curriculum.analyze.log.curriculum.pouring3.t1.lppour_datotaltip')
+    ct.Run('mysim.curriculum.analyze.log.curriculum.pouring3.t2.lppour_datotaltip')
+    ct.Run('mysim.curriculum.analyze.log.curriculum.pouring3.t3.lppour_datotaltip')
