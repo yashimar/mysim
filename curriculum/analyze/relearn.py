4baac37b8cb83d9277ae308cef797f807a089cc4
yashimar <ryota.yashima.s8@dc.tohoku.ac.jp>
2021-06-11 13:29:06 +0900

    suff->pref

--- curriculum/analyze/relearn.py
+++ curriculum/analyze/relearn.py
@@ -1,6 +1,6 @@
 # from core_tool import *
 from util import *
-from ..tasks_domain.pouring import task_domain as td
+from ..tasks_domain.detach_datotal.pouring import task_domain as td
 
 
 def Help():
@@ -8,16 +8,18 @@ def Help():
 
 
 def Run(ct, *args):
-    model_path = "curriculum/flow_ctrl/c_adaptive/curriculum_test/t1/c8_large_nobounce_tip_5_5_5_5"
-    save_path = "test/mms4"
-    model_name = "Ftip"
-    suff = ""
+    model_path = "curriculum3/pouring/full_scratch/curriculum_test/t1/first300"
+    save_path = "curriculum3/pouring/relearn/addone"
+    model_name = "Ftip_amount"
+    pref = ""
     
-    model, DataX, DataY = remake_model(td, model_name, model_path, save_path, suff)
+    model, DataX, DataY = remake_model(td, model_name, model_path, save_path, pref)
     print(model.Options)
     for x,y in zip(DataX, DataY):
+        x[2] += 1.
+        x[8] += 1.
         model.Update(x.tolist(),y.tolist(),not_learn=False)
     
     check_or_create_dir(ROOT_PATH+save_path)
-    with open(ROOT_PATH+save_path+"/{}_{}.pkl".format(model_name, suff), "wb") as f:
+    with open(ROOT_PATH+save_path+"/{}_{}.pkl".format(model_name, pref), "wb") as f:
         pickle.dump(model, f)
