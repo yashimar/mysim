9f207a8fa048d721e300335c50179cd35250cb26
yashimar <ryota.yashima.s8@dc.tohoku.ac.jp>
2021-06-11 21:05:01 +0900

    relearn関係の分析

--- curriculum/analyze/relearn.py
+++ curriculum/analyze/relearn.py
@@ -9,16 +9,15 @@ def Help():
 
 def Run(ct, *args):
     model_path = "curriculum3/pouring/full_scratch/curriculum_test/t1/first300"
-    save_path = "curriculum3/pouring/relearn/normal"
+    save_path = "curriculum3/pouring/relearn/nobounce_only"
     model_name = "Ftip_amount"
     pref = ""
     
     model, DataX, DataY = remake_model(td, model_name, model_path, save_path, pref, is_prev_model=True)
     print(model.Options)
     for x,y in zip(DataX, DataY):
-        # x[2] += 1.
-        # x[8] += 1.
-        model.Update(x.tolist(),y.tolist(),not_learn=False)
+        if x[5]==0.0:
+            model.Update(x.tolist(),y.tolist(),not_learn=False)
     
     check_or_create_dir(ROOT_PATH+save_path)
     with open(ROOT_PATH+save_path+"/{}_{}.pkl".format(model_name, pref), "wb") as f:
