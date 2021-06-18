2efbda2bd65be7ec9774282403b4aad50e88aecb
yashimar <ryota.yashima.s8@dc.tohoku.ac.jp>
2021-06-18 22:35:56 +0900

    Ftip_amount がちゃんと学習できているか確認用 (早期終了条件がゆるすぎて駄目)

--- curriculum/analyze/relearn.py
+++ curriculum/analyze/relearn.py
@@ -9,32 +9,31 @@ def Help():
 
 def Run(ct, *args):
     model_path = "curriculum5/c1/t5/g5"
-    save_path = "curriculum5/c1/t5/relearn/ampdatotal"
-    model_name = "Famount"
+    save_path = "curriculum5/c1/t5/relearn/Ftipamount_test"+str(args[0])
+    model_name = "Ftip_amount"
     pref = ""
     
-    # model, DataX, DataY = remake_model(td, model_name, model_path, save_path, pref, is_prev_model=True)
-    # print(model.Options)
-    # for x,y in zip(DataX, DataY):
-    #     x[8] *= 10
-    #     model.Update(x.tolist(),y.tolist(),not_learn=False)
+    model, DataX, DataY = remake_model(td, model_name, model_path, save_path, pref, is_prev_model=True)
+    print(model.Options)
+    for x,y in zip(DataX, DataY):
+        model.Update(x.tolist(),y.tolist(),not_learn=False)
     
-    # check_or_create_dir(ROOT_PATH+save_path)
-    # with open(ROOT_PATH+save_path+"/{}_{}.pkl".format(model_name, pref), "wb") as f:
-    #     pickle.dump(model, f)
+    check_or_create_dir(ROOT_PATH+save_path)
+    with open(ROOT_PATH+save_path+"/{}_{}.pkl".format(model_name, pref), "wb") as f:
+        pickle.dump(model, f)
         
         
-    with open(ROOT_PATH+"{}/{}_{}.pkl".format(save_path,model_name,pref), "rb") as f:
-        model = pickle.load(f)
+    # with open(ROOT_PATH+"{}/{}_{}.pkl".format(save_path,model_name,pref), "rb") as f:
+    #     model = pickle.load(f)
     
-    def pred_test(model):
-        for i, (x,t) in enumerate(zip(model.DataX, model.DataY)):
-            p = model.Predict(x, [0.,0.,0.,0.,0.,0.,0.,0.,0.3,0.,0.,0.], with_var=True)
-            # print("---")
-            # Print("input:", x)
-            # Print("est:", p.Y[0].item())
-            Print(i, "sigma:", np.sqrt(p.Var[0,0]))
-            # Print("true:", t[0])
-            # Print("diff:", abs(p.Y[0].item()-t[0]))
+    # def pred_test(model):
+    #     for i, (x,t) in enumerate(zip(model.DataX, model.DataY)):
+    #         p = model.Predict(x, [0.,0.,0.,0.,0.,0.,0.,0.,0.3,0.,0.,0.], with_var=True)
+    #         # print("---")
+    #         # Print("input:", x)
+    #         # Print("est:", p.Y[0].item())
+    #         Print(i, "sigma:", np.sqrt(p.Var[0,0]))
+    #         # Print("true:", t[0])
+    #         # Print("diff:", abs(p.Y[0].item()-t[0]))
         
-    pred_test(model)
+    # pred_test(model)
