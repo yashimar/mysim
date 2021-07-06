55652e62302348f98919cf661b4c2134b498bcd7
yashimar <ryota.yashima.s8@dc.tohoku.ac.jp>
2021-07-07 00:16:59 +0900

    飛び値の確認

--- curriculum/analyze/log/curriculum5/c1/trues_sampling/tip_ketchup_smsz_dtheta2/opttest/check.py
+++ curriculum/analyze/log/curriculum5/c1/trues_sampling/tip_ketchup_smsz_dtheta2/opttest/check.py
@@ -11,14 +11,22 @@ K10ERR = "k10_error"
 NNMEAN = "NN_mean"
 NNERR = "NN_error"
 NNSD = "NN_sd"
+JP1DIFF = "|JumpPoint - (NNmean+/-1NNerr)|"
+JP2DIFF = "|JumpPoint - (NNmean+/-2NNerr)|"
 BASE_DIR = "/home/yashima/ros_ws/ay_tools/ay_skill_extra/mysim/curriculum/analyze/log/curriculum5/c1/trues_sampling/tip_ketchup_smsz_dtheta2/"
 
+
+def idx_of_the_nearest(data, value):
+    idx = np.argmin(np.abs(np.array(data) - value))
+    return idx
+
 def Run(ct, *args):
-    name = "Er/t0.5"
+    name = "Er/t0.1"
+    save_img_dir = PICTURE_DIR + "opttest/{}/".format(name.replace("/","_"))
     
     logdir = BASE_DIR + "opttest/logs/{}/".format(name)
     dm = Domain.load(logdir+"dm.pickle")
-    if False:
+    if True:
         with open(logdir+"datotal.pickle", mode="rb") as f:
             datotal = pickle.load(f)
     else:
@@ -41,23 +49,42 @@ def Run(ct, *args):
         }
         with open(logdir+"datotal.pickle", mode="wb") as f:
             pickle.dump(datotal, f)
-        
+    
+    datotal[JP1DIFF], datotal[JP2DIFF] = np.ones((100,100))*(-100), np.ones((100,100))*(-100) 
+    for idx_dtheta2 in range(len(dm.dtheta2)):
+        for idx_smsz in range(len(dm.smsz)):
+            datotal[JP1DIFF][idx_dtheta2, idx_smsz] = max(
+                datotal[NNMEAN][idx_dtheta2, idx_smsz]-1*datotal[NNERR][idx_dtheta2, idx_smsz]-datotal[TRUE][idx_dtheta2, idx_smsz],
+                datotal[TRUE][idx_dtheta2, idx_smsz]-(datotal[NNMEAN][idx_dtheta2, idx_smsz]+1*datotal[NNERR][idx_dtheta2, idx_smsz]),
+                0
+            )
+            datotal[JP2DIFF][idx_dtheta2, idx_smsz] = max(
+                datotal[NNMEAN][idx_dtheta2, idx_smsz]-2*datotal[NNERR][idx_dtheta2, idx_smsz]-datotal[TRUE][idx_dtheta2, idx_smsz],
+                datotal[TRUE][idx_dtheta2, idx_smsz]-(datotal[NNMEAN][idx_dtheta2, idx_smsz]+2*datotal[NNERR][idx_dtheta2, idx_smsz]),
+                0
+            )
+    
     fig = make_subplots(
-        rows=3, cols=2, 
-        subplot_titles=["datotal 生データ (100×100)", "", "10×10平均", "|生データ - 10×10平均|", "NN平均予測", "NN誤差予測"],
+        rows=4, cols=2, 
+        subplot_titles=["datotal 生データ (100×100)", "", "10×10平均", "|生データ - 10×10平均|", "NN平均予測", "NN誤差予測", "|NN平均+/-NN誤差からのずれ|", "|NN平均+/-2NN誤差からのずれ|"],
         horizontal_spacing = 0.1,
         vertical_spacing = 0.1,
     )
     fig.update_layout(
-        height=1600, width=1750, 
+        height=2200, width=1750, 
         margin=dict(t=100,b=150),
         hoverdistance = 5,
     )
-    true_nnerr = [np.abs(v - dm.nnmodel.model.Predict([dtheta2,smsz]).Y.item()) for dtheta2, smsz, v in zip(dm.log["est_opt_dtheta2"], dm.log["smsz"], dm.log["true_datotal"])]
+    nnmean = [datotal[NNMEAN][idx_of_the_nearest(dm.dtheta2, dtheta2), idx_of_the_nearest(dm.smsz, smsz)] for dtheta2, smsz in zip(dm.log["est_opt_dtheta2"], dm.log["smsz"])]
+    nnerr = [datotal[NNERR][idx_of_the_nearest(dm.dtheta2, dtheta2), idx_of_the_nearest(dm.smsz, smsz)] for dtheta2, smsz in zip(dm.log["est_opt_dtheta2"], dm.log["smsz"])]
+    true_nnerr = [np.abs(v - _nnmean) for _nnmean, v in zip(nnmean, dm.log["true_datotal"])]
+    jp1diff = [max(_nnmean-1*_nnerr-v, v-(_nnmean+1*_nnerr), 0) for _nnmean, _nnerr, v in zip(nnmean, nnerr, dm.log["true_datotal"])]
+    jp2diff = [max(_nnmean-2*_nnerr-v, v-(_nnmean+2*_nnerr), 0) for _nnmean, _nnerr, v in zip(nnmean, nnerr, dm.log["true_datotal"])]
     z_rc_pos_scale_scatterz_scatterscale_set = (
-        (datotal[TRUE], 1, 1, 0.46, 0.87, 0., 0.55, None, None, None),
-        (datotal[K10MEAN], 2, 1, 0.46, 0.51, 0., 0.55, None, None, None), (datotal[K10ERR], 2, 2, 1.0075, 0.51, 0., 0.3, None, None, None),
-        (datotal[NNMEAN], 3, 1, 0.46, 0.15, 0., 0.55, dm.log["true_datotal"], 0., 0.55), (datotal[NNERR], 3, 2, 1.0075, 0.15, 0., 0.3, true_nnerr, 0., 0.3),
+        (datotal[TRUE], 1, 1, 0.46, 0.91, 0., 0.55, None, None, None),
+        (datotal[K10MEAN], 2, 1, 0.46, 0.63, 0., 0.55, None, None, None), (datotal[K10ERR], 2, 2, 1.0075, 0.63, 0., 0.3, None, None, None),
+        (datotal[NNMEAN], 3, 1, 0.46, 0.36, 0., 0.55, dm.log["true_datotal"], 0., 0.55), (datotal[NNERR], 3, 2, 1.0075, 0.36, 0., 0.36, true_nnerr, 0., 0.3),
+        (datotal[JP1DIFF], 4, 1, 0.46, 0.08, 0., 0.2, jp1diff, 0., 0.2), (datotal[JP2DIFF], 4, 2, 1.0075, 0.08, 0., 0.2, jp2diff, 0., 0.2),
     )
     for z, row, col, posx, posy, zmin, zmax, scz, sczmin, sczmax in z_rc_pos_scale_scatterz_scatterscale_set:
         fig.add_trace(go.Heatmap(
@@ -67,7 +94,7 @@ def Run(ct, *args):
             colorbar=dict(
                 titleside="top", ticks="outside",
                 x = posx, y = posy,
-                thickness=23, len = 0.3,
+                thickness=23, len = 0.19,
             ),
         ), row, col)
         if scz == None: continue
@@ -89,7 +116,95 @@ def Run(ct, *args):
                 )
             ),
         ), row, col)
-    for i in range(1,6):
+    for i in range(1,len(z_rc_pos_scale_scatterz_scatterscale_set)+2):
         fig['layout']['xaxis'+str(i)]['title'] = "size_srcmouth"
         fig['layout']['yaxis'+str(i)]['title'] = "dtheta2"
-    fig.show()
+    check_or_create_dir(save_img_dir)
+    plotly.offline.plot(fig, filename = save_img_dir + "heatmap.html", auto_open=False)
+    
+    
+    trace = defaultdict(list)
+    for smsz_idx, smsz in enumerate(dm.smsz):
+        trace[0].append(go.Scatter(
+            x=dm.dtheta2, y=datotal[NNMEAN][:,smsz_idx],
+            mode='lines', 
+            name="NNmean+/-1NNerr",
+            line=dict(color="orangered", dash="dash"),
+            visible=False,
+            error_y=dict(
+                type="data",
+                symmetric=True,
+                array=2*datotal[NNERR][:,smsz_idx],
+                color="orangered",
+                thickness=1.5,
+                width=3,
+            )
+        ))
+        trace[1].append(go.Scatter(
+            x=dm.dtheta2, y=datotal[NNMEAN][:,smsz_idx],
+            mode='lines', 
+            name="NNmean+/-2NNerr",
+            line=dict(color="orange", dash="dash"),
+            visible=False,
+            error_y=dict(
+                type="data",
+                symmetric=True,
+                array=datotal[NNERR][:,smsz_idx],
+                color="orange",
+                thickness=1.5,
+                width=3,
+            )
+        ))
+        trace[2].append(go.Scatter(
+            x=dm.dtheta2, y=datotal[TRUE][:,smsz_idx],
+            mode='markers', 
+            name="True datotal",
+            marker=dict(color="blue"),
+            visible=False,
+        ))
+        if smsz in dm.log["smsz"]:
+            log_smsz_idx_list = [log_smsz_idx for log_smsz_idx, log_smsz in enumerate(dm.log["smsz"]) if log_smsz == smsz]
+            trace[3].append(go.Scatter(
+                x=np.array(dm.log["est_opt_dtheta2"])[log_smsz_idx_list], y=np.array(dm.log["true_datotal"])[log_smsz_idx_list],
+                mode='markers', 
+                name="True datotal [TrainingData]",
+                marker=dict(color="purple", size=8),
+                visible=False,
+            ))
+        else:
+            trace[4].append(go.Scatter(x=[], y=[]))
+        
+    
+    for i in range(len(trace)):
+        trace[i][0].visible = True 
+    data = sum([trace[i] for i in range(len(trace))], [])   
+    
+    steps = []
+    for smsz_idx, smsz in enumerate(dm.smsz):
+        for j in range(len(trace)):
+            trace["vis{}".format(j)] = [False]*len(dm.smsz)
+            trace["vis{}".format(j)][smsz_idx] = True
+        step = dict(
+            method="update",
+            args=[{"visible": sum([trace["vis{}".format(k)] for k in range(len(trace))],[])},
+                {"title": "size_srcmouth: {:.4f}".format(smsz)}],
+        )
+        steps.append(step)
+    sliders = [dict(
+        active=10,
+        currentvalue={"prefix": "size_srcmouth: "},
+        pad={"t": 50},
+        steps=steps,
+    )]
+    
+    fig = go.Figure(data=data)
+    fig.update_layout(
+        sliders=sliders
+    )
+    fig['layout']['xaxis']['title'] = "dtheta2"
+    fig['layout']['yaxis']['title'] = "datotal"
+    fig['layout']['yaxis']['range'] = (-0.05,0.6)
+    for smsz_idx, smsz in enumerate(dm.smsz):
+        fig['layout']['sliders'][0]['steps'][smsz_idx]['label'] = round(smsz,4)
+    check_or_create_dir(save_img_dir)
+    plotly.offline.plot(fig, filename = save_img_dir + "curve.html", auto_open=False)
