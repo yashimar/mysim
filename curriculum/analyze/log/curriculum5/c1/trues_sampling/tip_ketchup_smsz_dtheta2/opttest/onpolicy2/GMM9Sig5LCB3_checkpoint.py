31d9950432edc255ce3d5b73a7589345d62badb0
yashimar <ryota.yashima.s8@dc.tohoku.ac.jp>
2021-08-14 20:54:20 +0900

    checkpointの実装

---
+++ curriculum/analyze/log/curriculum5/c1/trues_sampling/tip_ketchup_smsz_dtheta2/opttest/onpolicy2/GMM9Sig5LCB3_checkpoint.py
@@ -0,0 +1,28 @@
+#coding: UTF-8
+from ..learn3 import *
+
+def Help():
+    pass
+
+
+def Run(ct,*args):
+    base_logdir = "/home/yashima/ros_ws/ay_tools/ay_skill_extra/mysim/curriculum/analyze/log/curriculum5/c1/trues_sampling/tip_ketchup_smsz_dtheta2/opttest/"
+    name = "GMM9Sig5LCB3/checkpoints/t{}".format(args[0])
+    p = 5
+    options = {"tau": 0.9, "lam": 1e-6}
+    
+    execute_checkpoint(**dict(
+        num_ep = 500,
+        num_rand_sample = 10,
+        num_learn_step = 1,
+        num_checkpoints= 100,
+        
+        sd_gain = 1.0,
+        LCB_ratio = 3.0,
+        gmm_lams = {
+            TIP: lambda nnmodel: GMM9(nnmodel, diag_sigma=[(1.0-0.1)/(100./p), (0.8-0.3)/(100./p)], options = options, Gerr = 1.0),
+            SHAKE: lambda nnmodel: GMM9(nnmodel, diag_sigma=[(0.8-0.3)/(100./p)], options = options, Gerr = 1.0)
+        },
+        
+        base_logdir = base_logdir + "logs/onpolicy2/{}/".format(name),
+    ))
